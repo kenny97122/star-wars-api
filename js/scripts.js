1724641b34fadbab8bdc5ebe42d0a38310623625
kdelver <kenny.delver971@gmail.com>
2020-03-28 16:06:13 -0400

    Adding episodes

--- js/scripts.js
+++ js/scripts.js
@@ -11,23 +11,74 @@ const createEl = (element, className, text, parent) => {
 }
 
 const getCharacter = (characterName) => {
+
+    let loader = `
+          <div class="preloader-wrapper big active">
+      <div class="spinner-layer spinner-blue">
+        <div class="circle-clipper left">
+          <div class="circle"></div>
+        </div><div class="gap-patch">
+          <div class="circle"></div>
+        </div><div class="circle-clipper right">
+          <div class="circle"></div>
+        </div>
+      </div>
+    `;
+    infoDiv.innerHTML = loader
     fetch(`https://swapi.co/api/people/?search=${characterName}`)
         .then(data => data.json())
         .then(data => {
 
             infoDiv.innerHTML = ''
-            const resultCount = createEl('h2', 'results-count', `${data.count} results for "${characterName}"`)
+            const resultCount = createEl('h2', 'results-count', `${data.results.length} results for "${characterName}"`)
             console.log(data);
-            console.log(infoDiv);
-
             infoDiv.appendChild(resultCount)
             data.results.forEach(character => {
                 const list = createEl('ul', 'collection with-header', null)
                 const name = createEl('li', 'collection-header', ` <h4>  Name: ${character.name} </h4>`)
-                const height = createEl('li', 'collection-item', `Heigth: ${character.height}`)
-                const hairColor = createEl('li', 'collection-item', `Hair Color: ${character.hair_color}`)
-                const skinColor = createEl('li', 'collection-item', `Skin Color: ${character.skin_color}`)
-                const birthYear = createEl('li', 'collection-item', `Yearof Birt: ${character.birth_year}`)
+                const height = createEl('li', 'collection-item', `<b>Heigth:</b> ${character.height}`)
+                const hairColor = createEl('li', 'collection-item', `<b>Hair Color:</b> ${character.hair_color}`)
+                const skinColor = createEl('li', 'collection-item', `<b>Skin Color:</b> ${character.skin_color}`)
+                const birthYear = createEl('li', 'collection-item', `<b>Year of Birth:</b> ${character.birth_year}`)
+
+
+
+                fetch(character.homeworld)
+                    .then(data => data.json())
+                    .then(data => {
+                        console.log(`Second Data:`, data);
+                        const homeData = createEl('li', 'collection-item ', `<b>Home Name:</b>  ${data.name} <hr>`)
+                        const diameter = createEl('p', '', `<b>Diameter:</b> ${data.diameter}`)
+                        const climate = createEl('p', '', `<b>Climate: </b>${data.climate}`)
+                        const population = createEl('p', '', `<b>Population:</b> ${data.population}`)
+                        list.appendChild(homeData)
+                        homeData.appendChild(diameter)
+                        homeData.appendChild(climate)
+                        homeData.appendChild(population)
+                        const filmData = createEl('li', 'collection-item', `<b> Played in:</b> <hr>`)
+                        data.films.forEach(film => {
+                            fetch(film)
+                                .then(data => data.json())
+                                .then(data => {
+                                    console.log(data);
+
+                                    const filmTitle = createEl('p', '', `- ${data.title}`)
+                                    filmData.appendChild(filmTitle)
+                                    list.appendChild(filmData)
+
+                                })
+                                .catch(err => {
+                                    console.log(err);
+                                })
+                        })
+
+                    })
+                    .catch(err => {
+                        console.log(err);
+
+                    })
+
+
 
                 list.appendChild(name)
                 list.appendChild(height)
